f2f975248ded7dab9e6bb1378ebe00a70f21a370
Dmytro Volkov <v.d.a.9070@gmail.com>
2023-05-28 22:01:12 +0300

    number of active users

--- chat/consumers.py
+++ chat/consumers.py
@@ -4,18 +4,30 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.active_users = set()
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = "chat_%s" % self.room_name
 
         # Join room group
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-
+        self.active_users = []
+        self.active_user_channels = []
+        self.active_users.append(self.channel_name)
+        self.active_user_channels.append(
+            self.channel_name)
         await self.accept()
+        await self.send_active_users_count()
 
     async def disconnect(self, close_code):
         # Leave room group
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        self.active_users.remove(self.channel_name)
+        self.active_user_channels.remove(
+            self.channel_name)
+        await self.send_active_users_count()
 
     # Receive message from WebSocket
     async def receive(self, text_data=None, bytes_data=None):
@@ -36,3 +48,30 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({"message": message, "name": name}))
+
+        active_users_count = len(self.active_users)
+
+        for user_channel in self.active_users:
+            await self.channel_layer.send(
+                user_channel,
+                {
+                    "type": "update_active_users_count",
+                    "active_users_count": active_users_count,
+                }
+            )
+
+    async def send_active_users_count(self):
+        active_users_count = len(self.active_users)
+        for user_channel in self.active_user_channels:
+            await self.channel_layer.send(
+                user_channel,
+                {
+                    "type": "update_active_users_count",
+                    "active_users_count": active_users_count,
+                }
+            )
+
+    async def update_active_users_count(self, event):
+        active_users_count = event["active_users_count"]
+
+        await self.send(text_data=json.dumps({"active_users": active_users_count}))
